d6e686466987b9b9a6b24a3bcbfe1b6d328560be
liu.yalei <776248185@qq.com>
2020-06-16 10:28:23 +0800

    fix:自动部署github

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 构建
-npm run build
+npm run build:prod
 
 # cd 到构建输出的目录下
 cd dist
@@ -16,10 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io https://kuhami.github.io/KroInterview/
-git push -f git@github.com:kuhami master
+# 部署到 https://<USERNAME>.github.io
+#git push -f git@github.com:kuhami/vue-admin.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:kuhami/vue-admin.git master:gh-pages
 
 cd -
