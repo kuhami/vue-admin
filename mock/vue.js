55f7b462e7802adb874a9c28c72f4088fc217520
liu.yalei <776248185@qq.com>
2021-07-08 18:13:10 +0800

    fix:插槽和keep-alive

--- mock/vue.js
+++ mock/vue.js
@@ -52,21 +52,22 @@ const data = {
       creat_time: '2020-08-07',
       message:"总共分为8个阶段创建前/后，载入前/后，更新前/后，销毁前/后。<br clear='all' style=''/><br/>创建前/后： 在beforeCreated阶段，vue实例的挂载元素$el和**数据对象**data都为undefined，还未初始化。在created阶段，vue实例的数据对象data有了，$el还没有。<br/><br/>载入前/后：在beforeMount阶段，vue实例的$el和data都初始化了，但还是挂载之前为虚拟的dom节点，data.message还未替换。在mounted阶段，vue实例挂载完成，data.message成功渲染。<br/><br/>更新前/后：当data变化时，会触发beforeUpdate和updated方法。<br/><br/>销毁前/后：在执行destroy方法后，对data的改变不会再触发周期函数，说明此时vue实例已经解除了事件监听以及和dom的绑定，但是dom结构依然存在"
     },
-  //   {
-  //   id:'1',
-  //   title: '<keep-alive></keep-alive>的作用是什么?',
-  //   author: 'lyl',
-  //   status: '中',
-  //   creat_time: '2020-06-17',
-  //   message:'主要用于保留组件状态或避免重新渲染.'
-  // },{
-  //   id:'2',
-  //   title: 'slot（插槽）是干什么的?',
-  //   author: 'lyl',
-  //   status: '中',
-  //   creat_time: '2020-06-12',
-  //   message:'插槽含义：就是引入子组件后，在插入子组件元素中添加信息或者标签，使得子组件的指定位置插入信息或者标签; 插槽有三种：默认插槽、具名插槽、作用域插槽.'
-  // }
+    {
+      id:'6',
+      title: '（插槽）是干什么的?',
+      author: 'lyl',
+      status: '简单',
+      creat_time: '2020-07-08',
+      message:"插槽含义：就是引入子组件后，在插入子组件元素中添加信息或者标签，使得子组件的指定位置插入信息或者标签;<br/><br/> 插槽有三种：默认插槽、具名插槽、作用域插槽."
+    },
+    {
+      id:'7',
+      title: '<keep-alive/>的作用是什么?',
+      author: 'lyl',
+      status: '简单',
+      creat_time: '2020-07-08',
+      message:"主要用于保留组件状态或避免重新渲染."
+    },
   ]
 }
 
