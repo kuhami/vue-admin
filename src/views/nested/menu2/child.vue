7c0c1acdd1632682b6e4d21ab0dc0b76bc1f64c8
liu.yalei <776248185@qq.com>
2021-07-08 17:36:07 +0800

    fix:作用域插槽

---
+++ src/views/nested/menu2/child.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="list in lists">
+        <slot :bbbbb="list"></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'child',
+    props: ['lists'],
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -1,5 +1,32 @@
 <template>
   <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 2" />
+    <div>作用域插槽</div>
+    <child :lists="nameList">
+      <template slot-scope="a">
+        <div>你好：<span>{{a.bbbbb.name}}</span></div>
+      </template>
+    </child>
   </div>
 </template>
+
+<script>
+  import child from './child'
+
+  export default {
+    name: 'menu2',
+    components: {
+      child
+    },
+    data() {
+      return {
+        nameList: [
+          { id: 1, name: '孙悟空' },
+          { id: 2, name: '猪八戒' },
+          { id: 3, name: '沙和尚' },
+          { id: 4, name: '唐僧' },
+          { id: 5, name: '小白龙' }
+        ]
+      }
+    }
+  }
+</script>
