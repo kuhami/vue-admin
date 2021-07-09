ba0697a3f9d105efd698b0d35b7846aa550172c9
liu.yalei <776248185@qq.com>
2021-07-09 17:30:16 +0800

    fix:插槽 v-slots

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -6,16 +6,28 @@
         <div>你好：<span>{{a.bbbbb.name}}</span></div>
       </template>
     </child>
+    <div>v-slot</div>
+    <br>
+    <slot-name>
+      <template v-slot:header>我是新的header</template>
+      <template v-slot:main>哈哈哈哈</template>
+      <template v-slot:default>我是默认的</template>
+      <template v-slot:footer="slotProps">{{ slotProps.title.title }}---{{slotProps.message}}</template>
+
+      <!--<template #header>我是新的header001</template>-->
+    </slot-name>
   </div>
 </template>
 
 <script>
   import child from './child'
+  import slotName from './slotName'
 
   export default {
     name: 'menu2',
     components: {
-      child
+      child,
+      slotName
     },
     data() {
       return {
@@ -25,7 +37,7 @@
           { id: 3, name: '沙和尚' },
           { id: 4, name: '唐僧' },
           { id: 5, name: '小白龙' }
-        ]
+        ],
       }
     }
   }
---
+++ src/views/nested/menu2/slotName.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <slot name="header">我是header</slot>
+    <br>
+    <slot name="main">我是main</slot>
+    <br>
+    <slot :title="title" :message="message" name="footer">我是footer</slot>
+    <br>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'slotName',
+    data() {
+      return {
+        title: {
+          title: '我是slotName'
+        },
+        message: '我是Message'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
